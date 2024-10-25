b81ebacb096acd47d2405198461e97cada587b0a
SilverPulse <bogdansaimov78765@gmail.com>
2024-10-25 10:48:43 +0700

    Add files via upload

---
+++ Laba7/Lab7.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+void swap(char *start, char *end) {
+    char temp = *start;
+    *start = *end;
+    *end = temp;
+}
+void procWord(char *word) {
+    int len = strlen(word);
+    if (len > 1) {
+        swap(&word[0], &word[len - 1]);
+    }
+}
+int main() {
+    char text[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    
+    int i = 0;
+    char word[100];
+    int wordIndex = 0;
+
+    while (text[i] != '\0') {
+        if (isalnum(text[i])) {
+            word[wordIndex++] = text[i];
+        } else {
+            if (wordIndex > 0) {
+                word[wordIndex] = '\0';  // завершаем строку
+                procWord(word);  // обрабатываем слово
+                printf("%s", word);  // вывод обработанного слово
+                wordIndex = 0;  // сбрасываем индекс слова
+            }
+            printf("%c", text[i]);  // вывод символа, не являющийся частью слова
+        }
+        i++;
+    }
+
+    // Обработка последнего слова, если строка не заканчивается на пробел
+    if (wordIndex > 0) {
+        word[wordIndex] = '\0';
+        processWord(word);
+        printf("%s", word);
+    }
+    return 0;
+}
