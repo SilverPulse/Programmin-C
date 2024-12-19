460b45b0a05b1fb03842ae38ab7003dad91ae3fb
SilverPulse <bogdansaimov78765@gmail.com>
2024-12-19 10:26:46 +0700

    Add files via upload

---
+++ Laba12/cmake/bn/CMakeFiles/list-c.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/list-c.dir/list_files.c.o"
+  "CMakeFiles/list-c.dir/list_files.c.o.d"
+  "CMakeFiles/list-c.dir/main.c.o"
+  "CMakeFiles/list-c.dir/main.c.o.d"
+  "list-c"
+  "list-c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/list-c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Laba12/cmake/bn/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/Shaim/Desktop/Учеба/СибГути/Программирование/test
+# Build directory: /mnt/c/Users/Shaim/Desktop/Учеба/СибГути/Программирование/test/bn
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_list-c "/mnt/c/Users/Shaim/Desktop/Учеба/СибГути/Программирование/test/bn/list-c" "/")
+set_tests_properties(test_list-c PROPERTIES  WORKING_DIRECTORY "/mnt/c/Users/Shaim/Desktop/Учеба/СибГути/Программирование/test" _BACKTRACE_TRIPLES "/mnt/c/Users/Shaim/Desktop/Учеба/СибГути/Программирование/test/CMakeLists.txt;29;add_test;/mnt/c/Users/Shaim/Desktop/Учеба/СибГути/Программирование/test/CMakeLists.txt;0;")
