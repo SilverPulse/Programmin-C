58c9038c3962655801d14b78de6e4633a6c15cc1
SilverPulse <bogdansaimov78765@gmail.com>
2025-05-21 16:24:42 +0700

    Delete Sem_2_RGR/palindrom/build directory

--- Sem_2_RGR/palindrom/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/Shaim/Desktop/Учеба/СибГути/Программирование/Семестр 2/palindrom
-# Build directory: /mnt/c/Users/Shaim/Desktop/Учеба/СибГути/Программирование/Семестр 2/palindrom/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
